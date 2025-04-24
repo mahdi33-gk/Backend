201f2eaf712925d34ec8c4aa6e2c61bc00baf25c
mehedi <mehedi25122003@gmail.com>
2025-04-24 13:19:13 +0600

    form added

--- server-helper/src/Componets/Home.jsx
+++ server-helper/src/Componets/Home.jsx
@@ -8,9 +8,29 @@ const Home = () => {
         .then(res=>res.json())
         .then(data=>setUser(data));
     },[])
+    const submitHandler = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const name = form.name.value;
+        const user = {name,email}
+        console.log(user)
+    }
     return (
         <div>
             <h1>Welcome home {user.length}</h1>
+            <div>
+                <form onSubmit={submitHandler}>
+                    <input type="text" name="name" id="" />
+                    <br />
+                    <input type="email" name="email" id="" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            <div>
+                {user.map(single=> <p>{single.Roll}</p>)}
+            </div>
         </div>
     );
 };
