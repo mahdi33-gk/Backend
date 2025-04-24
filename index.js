478d9373ba6d64e3c84c430c00d36590b30238fa
mehedi <mehedi25122003@gmail.com>
2025-04-24 12:53:37 +0600

    react router set up

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const users =[
+    {name:'mehedi', Roll:5645},
+    {name:'mahadi', Roll:56454},
+]
 app.get('/',(req,res)=>{
     res.send('gotcha bruh!')
+})
+app.get('/users',(req,res)=>{
+    res.send(users)
 })
 app.listen(port,()=>{
     console.log(port)
