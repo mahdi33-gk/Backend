1a6d36208b5c9626d71bb258dec672fca6c64b40
mehedi <mehedi25122003@gmail.com>
2025-04-26 22:07:58 +0600

    bug fixed

--- module2/server-site-helper/src/Users.jsx
+++ module2/server-site-helper/src/Users.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const data = useLoaderData();
-    return (
-        <div>
-            <div>
-                <h1>{data.length}</h1>
-                {data.map(singledata=> <h1 key={singledata._id}>{singledata.name} n {singledata.email}</h1>)}
-            </div>
-        </div>
-    );
+  const data = useLoaderData();
+  return (
+    <div>
+      <div>
+        <h1>{data.length}</h1>
+        {
+        data.map(singledata => 
+          <div key={singledata._id}>
+            {singledata.name} : {singledata.email} <button>x</button>{" "}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
